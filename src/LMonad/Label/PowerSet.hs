89a7168a37c2d743e86c8e90e6b4a12611ccba51
James Parker <dev@jamesparker.me>
2014-10-08 14:32:17 -0400

    renamed PSLabel

--- src/LMonad/Label/PowerSet.hs
+++ src/LMonad/Label/PowerSet.hs
@@ -7,32 +7,32 @@ import Prelude
 import LMonad
 
 -- | Power set label made of all combinations of the principals. 
-data Ord p => PowerSetLabel p = PowerSetLabel {
+data Ord p => PSLabel p = PSLabel {
         powerSetLabelConfidentiality :: Set p
       , powerSetLabelIntegrity :: Set p
     }
         deriving Show
 
-instance Ord p => Label (PowerSetLabel p) where
+instance Ord p => Label (PSLabel p) where
     -- Meet
-    glb (PowerSetLabel c1 i1) (PowerSetLabel c2 i2) =   
+    glb (PSLabel c1 i1) (PSLabel c2 i2) =   
         let c = Set.intersection c1 c2 in
         let i = Set.intersection i1 i2 in
-        PowerSetLabel c i
+        PSLabel c i
 
     -- Join
-    lub (PowerSetLabel c1 i1) (PowerSetLabel c2 i2) =   
+    lub (PSLabel c1 i1) (PSLabel c2 i2) =   
         let c = Set.union c1 c2 in
         let i = Set.union i1 i2 in
-        PowerSetLabel c i
+        PSLabel c i
 
     -- canFlowTo
-    canFlowTo (PowerSetLabel c1 i1) (PowerSetLabel c2 i2) =
+    canFlowTo (PSLabel c1 i1) (PSLabel c2 i2) =
         (Set.isSubsetOf c1 c2) && (Set.isSubsetOf i1 i2)
 
     -- Bottom
     bottom = 
-        PowerSetLabel Set.empty Set.empty
+        PSLabel Set.empty Set.empty
 
 -- | Type alias for labeled power sets.
-type PowerSetLabeled p = Labeled (PowerSetLabel p)
+type PSLabeled p = Labeled (PSLabel p)
