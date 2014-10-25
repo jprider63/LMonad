3bc4dc58f90e74c9220462849ca333952df61a34
James Parker <dev@jamesparker.me>
2014-10-25 19:39:27 -0400

    tests for DCLabel canFlowTo

--- test/DCLabelTest.hs
+++ test/DCLabelTest.hs
@@ -21,6 +21,9 @@ l1 = dcConfidentialitySingleton 4
 l2 :: DCLabel Int
 l2 = dcIntegritySingleton 4
 
+l3 :: DCLabel Int
+l3 = dcSingleton 4
+
 joinTest :: Test
 joinTest = TestCase $ do
     assertEqual "bottom `lub` l /= l" l $ b `lub` l
@@ -43,9 +46,29 @@ meetTest = TestCase $ do
 
     assertEqual "l `glb` l /= l" l $ l `glb` l
 
+canFlowToTest :: Test
+canFlowToTest = TestCase $ do
+    assertBool "bottom `canFlowTo` l1" $ b `canFlowTo` l1
+    assertBool "not (l1 `canFlowTo` bottom)" $ not $ l1 `canFlowTo` b
+
+    assertBool "bottom `canFlowTo` l2" $ b `canFlowTo` l2
+    assertBool "not (l2 `canFlowTo` bottom)" $ not $ l2 `canFlowTo` b
+
+    assertBool "bottom `canFlowTo` l" $ b `canFlowTo` l
+    assertBool "not (l `canFlowTo` bottom)" $ not $ l `canFlowTo` b
+
+    assertBool "bottom `canFlowTo` l'" $ b `canFlowTo` l'
+    assertBool "not (l' `canFlowTo` bottom)" $ not $ l' `canFlowTo` b
+
+    assertBool "l1 `canFlowTo l3" $ not $ l1 `canFlowTo` l3
+    assertBool "not (l3 `canFlowTo l1)" $ not $ l3 `canFlowTo` l1
+
+    assertBool "l2 `canFlowTo l3" $ not $ l1 `canFlowTo` l3
+    assertBool "not (l2 `canFlowTo l1)" $ not $ l3 `canFlowTo` l1
+
 tests :: Test
 tests = TestLabel "DCLabel tests" $ TestList [
        TestLabel "Test join" joinTest
      , TestLabel "Test meet" meetTest
---      , TestLabel "Test canFlowTo" canFlowToTest
+     , TestLabel "Test canFlowTo" canFlowToTest
     ]
